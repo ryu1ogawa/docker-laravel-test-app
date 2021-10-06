1d590ba1994b5ce9b8bd274bb2ccc586db68d358
nmdz8eyc7ab <nmdz8eyc7ab@gmail.com>
2021-10-06 20:36:55 +0900

    created routing to app.blade.php

---
+++ backend/resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+</head>
+<body>
+    <header class="container d-flex justify-content-between">
+    <h1>
+        <a href="/">Test App</a>
+    </h1>
+    <nav class="nav">
+        <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">LOGIN</a></li>
+            
+        </ul>
+    </nav>
+    </header>
+    <main></main>
+    <footer></footer>
+    
+</body>
