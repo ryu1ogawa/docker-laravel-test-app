ee77583987b6ac474bdefa2cb41dc21aba55a4b8
nmdz8eyc7ab <nmdz8eyc7ab@gmail.com>
2021-10-10 19:44:07 +0900

    edited app.blade.php

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
       <div class="navbar-nav">
         <a class="nav-link active" aria-current="page" href="#">Home</a>
         <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
+        
         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
       </div>
     </div>
