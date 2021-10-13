f76dbaa01c02a808b27771a54d2983e333c43612
nmdz8eyc7ab <nmdz8eyc7ab@gmail.com>
2021-10-13 20:32:13 +0900

    creating login function

---
+++ backend/resources/views/requests/member_contract_registration.blade.php
@@ -0,0 +1,8 @@
+<form>
+    <div class="input-group mb-3">
+        <label for="memberContract" class="form-label">契約種</label>
+        <input id="memberContract" type="text" class="form-control" placeholder="" aria-label="memberContract">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
