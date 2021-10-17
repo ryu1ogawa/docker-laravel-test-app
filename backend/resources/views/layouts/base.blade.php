df0b3c3a445524f8880424e8f42603be443d9219
nmdz8eyc7ab <nmdz8eyc7ab@gmail.com>
2021-10-18 06:20:55 +0900

    creating base.blade.php

---
+++ backend/resources/views/layouts/base.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css">
+
+</head>
+
+<body>
+
+  <main class="d-flex">
+    
+    <div class="container"></div>
+  </main>
+  <footer></footer>
+
+</body>
