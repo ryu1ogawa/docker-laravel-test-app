22b3fdc05df8e93ffdb15d54865f20c8a90cca8d
nmdz8eyc7ab <nmdz8eyc7ab@gmail.com>
2021-10-20 20:27:50 +0900

    creating register.blade.php

--- backend/resources/views/auth/register.blade.php
+++ backend/resources/views/auth/register.blade.php
@@ -3,58 +3,99 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">新規登録</div>
 
-                <div class="card-body">
+                <div class="card-body p-3">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                        <div class="form-group row mb-2">
+                            <label for="last-name" class="col-md-4 col-form-label text-md-right">姓</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="last-name" type="text" placeholder="山田" class="form-control @error('first-name') is-invalid @enderror" name="last-name" required autocomplete="last-name">
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                @error('last-name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        
+                        <div class="form-group row mb-2">
+                            <label for="first-name" class="col-md-4 col-form-label text-md-right">名</label>
+
+                            <div class="col-md-6">
+                                <input id="first-name" type="text" placeholder="太郎" class="form-control @error('first-name') is-invalid @enderror" name="first-name" required autocomplete="first-name">
+
+                                @error('first-name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        
+                        <div class="form-group row mb-2">
+                            <label for="last-name-reading" class="col-md-4 col-form-label text-md-right">せい</label>
+
+                            <div class="col-md-6">
+                                <input id="last-name-reading" type="text" placeholder="やまだ" class="form-control @error('first-name-reading') is-invalid @enderror" name="last-name-reading" required autocomplete="last-name-reading">
+
+                                @error('last-name-reading')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        
+                        <div class="form-group row mb-2">
+                            <label for="first-name-reading" class="col-md-4 col-form-label text-md-right">めい</label>
+
+                            <div class="col-md-6">
+                                <input id="first-name-reading" type="text" placeholder="たろう" class="form-control @error('first-name-reading') is-invalid @enderror" name="first-name-reading" required autocomplete="first-name-reading">
+
+                                @error('first-name-reading')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="form-group row mb-2">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" placeholder="test@example.com"class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <div class="form-group row mb-2">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">パスワード<span class="text-danger">※半角英数字8文字以上</span></label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                        <div class="form-group row mb-2">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">パスワード(確認用)</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -64,7 +105,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    登録
                                 </button>
                             </div>
                         </div>
