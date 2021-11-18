d6a1f1e8328864ef39c4004b9a073f216eb6e101
nmdz8eyc7ab <nmdz8eyc7ab@gmail.com>
2021-11-18 23:32:40 +0900

    created sidebar.blade.php of employee

---
+++ backend/resources/views/logged_in/employee/sidebar.blade.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="register">
+        <ul>
+            <li><a href="{{ route('classification.create') }}">対象学年区分</a></li>
+            <li><a href="{{ route('contract_kind.create') }}">会員種別</a></li>
+            <li><a href="{{ route('course.create') }}">コース</a></li>
+            <li><a href="{{ route('education_facility.create') }}">教育機関</a></li>
+            <li><a href="{{ route('school_name.create') }}">学校名</a></li>
+        </ul>
+
+    </div>
+</div>
