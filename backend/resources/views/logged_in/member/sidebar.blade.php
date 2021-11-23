fc46bb5cb36de924afc0fdc5e20ed27c60e7529a
nmdz8eyc7ab <nmdz8eyc7ab@gmail.com>
2021-11-23 21:30:24 +0900

    creating member sidebar.blade and common edit_member.blade

---
+++ backend/resources/views/logged_in/member/sidebar.blade.php
@@ -0,0 +1,12 @@
+@extends('logged_in.employee.')
+
+
+<div class="container">
+    <div class="register">
+        <ul>
+            <li><a href="">会員情報変更</a></li>
+            
+        </ul>
+
+    </div>
+</div>
